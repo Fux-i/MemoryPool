f2143fa3060562b238d662a0de851ceccdb03f67
fuxi <1538130391@qq.com>
2025-10-27 21:28:26 +0800

    ✨feat: PageCache

---
+++ src/MemoryPool_v2/PageCache.cpp
@@ -0,0 +1,160 @@
+﻿#include "PageCache.h"
+
+namespace MemoryPoolV2
+{
+
+std::optional<MemorySpan> PageCache::AllocatePage(size_t pageCount) 
+{
+	if (pageCount == 0) return std::nullopt;
+	std::lock_guard guard(mutex_);
+	// find first element that greater than or equal to given one
+	auto it = freePageStore_.lower_bound(pageCount);
+	while (it != freePageStore_.end())
+	{
+		// find first valid page
+		if (it->second.empty()) ++it;
+		
+		auto spanIt = it->second.begin();
+		MemorySpan freeSpan = *spanIt;
+		// delete from store and map
+		it->second.erase(spanIt);
+		freePageMap_.erase(freeSpan.GetData());
+		// slice
+		size_t sizeToUse = pageCount * SizeUtil::PAGE_SIZE;
+		MemorySpan memoryToUse = freeSpan.SubSpan(0, sizeToUse);
+		// rest free memory
+		freeSpan = freeSpan.SubSpan(sizeToUse);
+		if (freeSpan.GetSize())
+		{
+			freePageStore_[freeSpan.GetSize() / SizeUtil::PAGE_SIZE].emplace(freeSpan);
+			freePageMap_.emplace(freeSpan.GetData(), freeSpan);
+		}
+		return memoryToUse;
+	}
+
+	// no page of this size
+	size_t pageToAllocate = std::max(PAGE_ALLOCATE_COUNT, pageCount);
+	return SystemAlloc(pageToAllocate).transform([this, pageCount](MemorySpan memory)
+	{
+		pageVector_.push_back(memory);
+		size_t memoryToUse = pageCount * SizeUtil::PAGE_SIZE;
+		MemorySpan result = memory.SubSpan(0, memoryToUse);
+		// rest free memory
+		MemorySpan freeSpan = memory.SubSpan(memoryToUse);
+		if (freeSpan.GetSize())
+		{
+			freePageStore_[freeSpan.GetSize() / SizeUtil::PAGE_SIZE].emplace(freeSpan);
+			freePageMap_.emplace(freeSpan.GetData(), freeSpan);
+		}
+		return result;
+	});
+}
+
+void PageCache::DeallocatePage(MemorySpan page) 
+{
+	assert(page.GetSize() % SizeUtil::PAGE_SIZE == 0);
+	std::lock_guard guard(mutex_);
+
+	// check previous memory spans, combine if continuous
+	while (!freePageMap_.empty())
+	{
+		// page should not be contain
+		assert(!freePageMap_.contains(page.GetData()));
+		// find first element that greater than given one
+		auto it = freePageMap_.upper_bound(page.GetData());
+		if (it != freePageMap_.begin())
+		{
+			--it;	// check previous one
+			const MemorySpan& prevSpan = it->second;
+			if (prevSpan.GetData() + prevSpan.GetSize() == page.GetData())
+			{
+				// combine continuous ones;
+				page = MemorySpan(prevSpan.GetData(), prevSpan.GetSize() + page.GetSize());
+				// delete from store and map
+				freePageStore_[prevSpan.GetSize() / SizeUtil::PAGE_SIZE].erase(prevSpan);
+				freePageMap_.erase(it);
+			}
+			else break;	// not continuous
+		}
+		else break;	// no more previous
+	}
+
+	// check memory spans after the page
+	while (!freePageMap_.empty())
+	{
+		// page should not be contained
+		assert(!freePageMap_.contains(page.GetData()));
+		if (freePageMap_.contains(page.GetData() + page.GetSize()))
+		{
+			auto it = freePageMap_.find(page.GetData() + page.GetSize());
+			MemorySpan nextSpan = it->second;
+			freePageStore_[nextSpan.GetSize() / SizeUtil::PAGE_SIZE].erase(nextSpan);
+			freePageMap_.erase(it);
+			page = MemorySpan(page.GetData(), page.GetSize() + nextSpan.GetSize());
+		}
+		else break;
+	}
+	const size_t index = page.GetSize() / SizeUtil::PAGE_SIZE;
+	freePageStore_[index].emplace(page);
+	freePageMap_.emplace(page.GetData(), page);
+	
+}
+
+std::optional<MemorySpan> PageCache::AllocateUnit(size_t size) 
+{
+	auto ptr = malloc(size);
+	if (ptr == nullptr)
+		return std::nullopt;
+	return std::make_optional<MemorySpan>(static_cast<std::byte*>(ptr), size);
+}
+
+void PageCache::DeallocateUnit(MemorySpan memoryUnit) 
+{
+	free(memoryUnit.GetData());
+}
+
+void PageCache::Stop() 
+{
+	std::lock_guard guard(mutex_);
+	if (!isStop_)
+	{
+		isStop_ = true;
+		for (const auto& page : pageVector_)
+			SystemFree(page);
+	}
+}
+
+#ifdef _WIN32
+#include <windows.h>
+#elif defined(__linux__) || defined(__APPLE__)
+#include <sys/mman.h>
+#else
+#error "Unsupported platform"
+#endif
+
+std::optional<MemorySpan> PageCache::SystemAlloc(const size_t pageCount) 
+{
+	const size_t size = SizeUtil::PAGE_SIZE * pageCount;
+	void* ptr;
+#ifdef _WIN32
+	ptr = VirtualAlloc(nullptr, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+#elif defined(__linux__) || defined(__APPLE__)
+	ptr =  mmap(nullptr, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+#endif
+	if (ptr == nullptr)
+		return std::nullopt;
+
+	memset(ptr, 0, size);
+	return std::make_optional<MemorySpan>(static_cast<std::byte*>(ptr), size);
+}
+
+void PageCache::SystemFree(MemorySpan pages) 
+{
+#ifdef _WIN32
+	VirtualFree(pages.GetData(), 0, MEM_RELEASE);
+#elif defined(__linux__) || defined(__APPLE__)
+	mmap(pages.GetData(), pages.GetSize());
+#endif
+}
+
+}
---
+++ src/MemoryPool_v2/PageCache.h
@@ -0,0 +1,78 @@
+﻿#pragma once
+
+#include <map>
+#include <mutex>
+#include <optional>
+#include <set>
+#include <vector>
+
+#include "Common.h"
+
+namespace MemoryPoolV2
+{
+
+class PageCache
+{
+public:
+	// delete constructor
+	PageCache() = delete;
+	
+	static PageCache& GetInstance()
+	{
+		thread_local PageCache instance;
+		return instance;
+	}
+
+	/**
+	 * Allocate pages from system
+	 * @param pageCount number of pages to allocate
+	 * @return optional memory span
+	 */
+	std::optional<MemorySpan> AllocatePage(size_t pageCount);
+
+	/**
+	 * Deallocate pages to system
+	 * @param page memory span
+	 */
+	void DeallocatePage(MemorySpan page);
+
+	/**
+	 * Request a large block memory
+	 * @param size memory size
+	 * @return optional memory span
+	 */
+	std::optional<MemorySpan> AllocateUnit(size_t size);
+
+	/**
+	 * Deallocate memory unit to system
+	 * @param memoryUnit memory span
+	 */
+	void DeallocateUnit(MemorySpan memoryUnit);
+	
+	// Stop working
+	void Stop();
+private:
+
+	/**
+	 * System allocate memory
+	 * @param pageCount memory page count
+	 * @return optional memory span
+	 */
+	std::optional<MemorySpan> SystemAlloc(size_t pageCount);
+
+	/**
+	 * System free memory
+	 * @param pages memory span
+	 */
+	void SystemFree(MemorySpan pages);
+
+	static constexpr size_t PAGE_ALLOCATE_COUNT = 2048;
+	
+	std::map<size_t, std::set<MemorySpan>> freePageStore_;	// [page count - memory span] map, for allocate
+	std::map<std::byte*, MemorySpan> freePageMap_;	// [ptr = memory span] map, for deallocate
+	std::vector<MemorySpan> pageVector_;
+	bool isStop_ = false;
+	std::mutex mutex_;
+};
+
+}
