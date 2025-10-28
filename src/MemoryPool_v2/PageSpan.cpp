221f6502ba9fb8ea4c1eda208b7fa0fbab5b8e31
fuxi <1538130391@qq.com>
2025-10-28 20:53:57 +0800

    ✨feat: PageSpan

---
+++ src/MemoryPool_v2/PageSpan.cpp
@@ -0,0 +1,36 @@
+﻿#include "PageSpan.h"
+
+namespace MemoryPoolV2
+{
+
+void PageSpan::Allocate(const MemorySpan memory)
+{
+	// check if the memory is in charge
+	assert(IsInCharge(memory));
+	const ptrdiff_t addressOffset = memory.GetData() - memory_.GetData();
+	const size_t index = addressOffset / unitSize_;
+	assert(allocatedMap_[index] == 0);
+	allocatedMap_[index] = true;
+}
+
+void PageSpan::Deallocate(const MemorySpan memory)
+{
+	// check if the memory is in charge
+	assert(IsInCharge(memory));
+	const ptrdiff_t addressOffset = memory.GetData() - memory_.GetData();
+	const size_t index = addressOffset / unitSize_;
+	assert(allocatedMap_[index] == 1);
+	allocatedMap_[index] = false;
+}
+
+bool PageSpan::IsInCharge(const MemorySpan memory) const
+{
+	if (memory.GetSize() != unitSize_) return false;
+	if (memory.GetData() < memory_.GetData()) return false;
+	// check start address
+	const ptrdiff_t addressOffset = memory.GetData() - memory_.GetData();
+	if (addressOffset % unitSize_ != 0) return false;
+	// check end address
+	return addressOffset + unitSize_ <= memory_.GetSize();
+}
+}
---
+++ src/MemoryPool_v2/PageSpan.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include <bitset>
+
+#include "Common.h"
+
+namespace MemoryPoolV2
+{
+
+class PageSpan
+{
+public:
+	PageSpan(const MemorySpan span, const size_t unitSize): memory_(span), unitSize_(unitSize) {}
+
+	// generate comparison operators
+	auto operator<=>(const PageSpan& other) const { return memory_.GetData() <=> other.memory_.GetData(); }
+	
+	void Allocate(MemorySpan memory);
+	void Deallocate(MemorySpan memory);
+
+	[[nodiscard]] bool empty() const { return allocatedMap_.none(); }
+	[[nodiscard]] bool IsInCharge(MemorySpan memory) const;
+
+	[[nodiscard]] size_t GetSize() const { return memory_.GetSize(); }
+	[[nodiscard]] std::byte* GetData() const { return memory_.GetData(); }
+	[[nodiscard]] size_t GetUnitSize() const { return unitSize_; }
+	[[nodiscard]] MemorySpan GetMemorySpan() const { return memory_; }
+
+private:
+	static constexpr size_t MAX_UNIT_COUNT = SizeUtil::PAGE_SIZE / SizeUtil::ALIGNMENT;
+
+	MemorySpan memory_;
+	size_t unitSize_;
+	// allocating status
+	std::bitset<MAX_UNIT_COUNT> allocatedMap_;
+};
+
+}
+
